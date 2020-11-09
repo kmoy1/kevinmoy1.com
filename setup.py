85f00f49642a66378aad75dc426a1546a0462d2d
Kevin Moy <kevinmoy@berkeley.edu>
2020-11-08 20:23:37 -0800

    test

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='kevinmoy1.com',
+    version='1.0',
+    long_description=__doc__,
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask']
+)
