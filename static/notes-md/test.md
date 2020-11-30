0a8c5eee3e901aa3c5e375627776c8c8164a67b5
Kevin Moy <kevinmoy@berkeley.edu>
2020-11-30 04:14:58 -0800

    Fully implemented markdown with latex parsing, code with syntax highlighting!

---
+++ static/notes-md/test.md
@@ -0,0 +1,35 @@
+Welcome to a sample article. This is just a sample. I repeat: this is just a sample.Welcome to a sample article. This is just a sample. I repeat: this is just a sample.Welcome to a sample article. This is just a sample. I repeat: this is just a sample.Welcome to a sample article. This is just a sample. I repeat: this is just a sample.Welcome to a sample article. This is just a sample. I repeat: this is just a sample.Welcome to a sample article. This is just a sample. I repeat: this is just a sample.Welcome to a sample article. This is just a sample. I repeat: this is just a sample.
+
+Testing words.
+
+`Hello World`.
+
+Some Latex: $ f(x) = (x+1)^2 $
+
+<ol>
+    <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+</ol>
+
+Super Latex: 
+
+$$\sum_{x=1}^{10}x^2$$
+
+
+```Python
+from flask import Flask, render_template, request, jsonify
+import re
+
+class Config(object):
+    SECRET_KEY = '78w0o5tuuGex5Ktk8VvVDF9Pw3jv1MVE'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+x = 1
+y = 2
+x, y = y, x
+```
+
+
--- test_routes.py
+++ test_routes.py
@@ -1,7 +1,29 @@
-from app import app, render_template, request
-from flask import Flask, request, render_template
+from flask_misaka import Misaka, markdown
+from misaka import Markdown, HtmlRenderer
+from pygments import highlight
+from pygments.formatters import HtmlFormatter, ClassNotFound
+from pygments.lexers import get_lexer_by_name
 
+from flask import Flask, render_template, request
+# from flaskext.markdown import Markdown
+from app import app, render_template, request
+
+class HighlighterRenderer(HtmlRenderer):
+   def blockcode(self, text, lang):
+      try:
+         lexer = get_lexer_by_name(lang, stripall=True)
+      except ClassNotFound:
+         lexer = None
+
+      if lexer:
+         formatter = HtmlFormatter()
+         return highlight(text, lexer, formatter)
+      # default
+      return '\n<pre><code>{}</code></pre>\n'.format(text)
+
+Misaka(app)
 
 @app.route('/test')
 def test():
-   return render_template('test.html')
+   md_file = open("static/notes-md/test.md", "r").read()
+   return render_template('test.html', md_file = markdown(md_file, fenced_code=True, math=True, math_explicit=True))
