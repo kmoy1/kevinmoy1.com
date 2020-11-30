0e15817440bd555d6fb7badce6fe20c9d50d9f68
Kevin Moy <kevinmoy@berkeley.edu>
2020-11-30 04:20:54 -0800

    Done a LOT

--- test_routes.py
+++ test_routes.py
@@ -1,5 +1,4 @@
 from flask_misaka import Misaka, markdown
-from misaka import Markdown, HtmlRenderer
 from pygments import highlight
 from pygments.formatters import HtmlFormatter, ClassNotFound
 from pygments.lexers import get_lexer_by_name
@@ -7,19 +6,6 @@ from pygments.lexers import get_lexer_by_name
 from flask import Flask, render_template, request
 # from flaskext.markdown import Markdown
 from app import app, render_template, request
-
-class HighlighterRenderer(HtmlRenderer):
-   def blockcode(self, text, lang):
-      try:
-         lexer = get_lexer_by_name(lang, stripall=True)
-      except ClassNotFound:
-         lexer = None
-
-      if lexer:
-         formatter = HtmlFormatter()
-         return highlight(text, lexer, formatter)
-      # default
-      return '\n<pre><code>{}</code></pre>\n'.format(text)
 
 Misaka(app)
 
