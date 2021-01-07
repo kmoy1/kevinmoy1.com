37ef923777f8dd7b2b20e1b2c6b193140230a37e
Kevin Moy <kevinmoy@berkeley.edu>
2021-01-06 19:46:44 -0700

    Fixed note hierarchy and ordering

--- notes_routes.py
+++ notes_routes.py
@@ -5,15 +5,20 @@ from flask_misaka import Misaka, markdown
 from flask import Flask, render_template, request
 Misaka(app)
 
+def filenameFormat(filename):
+    f = filename.replace("_", " ")
+    f = f.replace(".pdf", "")
+    f = f.replace("-", ":")
+    return f
+
 # Note index L2: By subject (LATEX)
 @app.route('/notes/notes-lt')
 def latexnotes_subject_index():
-    sub_lst = []
     routes = []
-    for subject in os.listdir("static/notes"):
-        sub_lst.append(subject)
+    subject_names = ["CS61B: Data Structures", "CS61C: Machine Architectures"]
+    for subject in os.listdir("static/notes-lt"):
         routes.append("/notes/notes-lt/" + subject)
-    return render_template('/notes/notes_subject_index.html', len = len(sub_lst), subjects=sub_lst, routes=routes)
+    return render_template('/notes/notes_subject_index.html', len = len(subject_names), subjects=subject_names, routes=routes)
 
 #Note index L2: By subject (MARKDOWN)
 @app.route('/notes/notes-md')
@@ -26,13 +31,17 @@ def mdnotes_subject_index():
     return render_template('/notes/notes_subject_index.html', len = len(sub_lst), subjects=sub_lst, routes=routes)
 
 #Note index L3: By note (given subject) (LATEX)
+#Note filename MUST be in format Note_{note number}-_{title}
 @app.route('/notes/notes-lt/<subject>')
 def latexnotes_note_index(subject):
-    notes_lst = []
-    for note in os.listdir("static/notes/" + subject):
-        notes_lst.append(note)
-    notes_lst.sort()
-    return render_template('/notes/notes_note_index.html', subject=subject, N=len(notes_lst), notes_lst = notes_lst, latex="True")
+    note_names = []
+    note_filenames = []
+    notes_lst = os.listdir("static/notes-lt/" + subject)
+    notes_lst.sort(key = lambda x: float(x.split('_')[1].replace('-','')))
+    for note in notes_lst:
+        note_names.append(filenameFormat(note))
+        note_filenames.append(note)
+    return render_template('/notes/notes_note_index.html', subject=subject, N=len(note_names), notes_lst = note_names, note_filenames = note_filenames, latex="True")
 
 #Note index L3: By note (given subject) (MARKDOWN)
 @app.route('/notes/notes-md/<path:path>')
@@ -41,7 +50,7 @@ def md_indexer(path):
     if os.path.isdir("static/notes-md/" + path):#Path = directory.
         for note in os.listdir("static/notes-md/" + path):
             notes_lst.append(note)
-        notes_lst.sort()
+        # notes_lst.sort()
         return render_template('/notes/notes_note_index.html', subject=path, N=len(notes_lst), notes_lst = notes_lst)
     else: #Path
         filename = "static/notes-md/" + path
