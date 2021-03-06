cee11edb2cf2826ae7a854439c6d599294ba3dd0
Kevin Moy <kevinmoy@berkeley.edu>
2021-03-06 13:52:46 -0800

    Added multiple choice design

--- test_routes.py
+++ test_routes.py
@@ -7,8 +7,7 @@ Misaka(app)
 
 @app.route('/test')
 def test():
-   md_file = open("static/notes-md/test.md", "r").read()
-   return render_template('test.html', md_file = markdown(md_file, fenced_code=True, math=True, math_explicit=True))
+   return render_template('test.html')
 
 @app.route('/mcq-test')
 def mcq_test():
